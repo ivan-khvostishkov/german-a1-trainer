470e44e1ca536d99e6481d7a26191cb3c50a8bf9
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-07-15 09:39:15 +0200

    Attach CodeWhisperer

--- src/main/net/nosocial/germana1/BuildAudio.java
+++ src/main/net/nosocial/germana1/BuildAudio.java
@@ -3,5 +3,8 @@ package net.nosocial.germana1;
 public class BuildAudio {
     public static void main(String[] args) {
         System.out.println("German A1 Trainer Tool (c) 2023 by NoSocial.Net");
+
+        // download file and save to S3
+
     }
 }
