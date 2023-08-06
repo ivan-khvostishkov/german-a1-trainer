80a3e59830c28c731aa4175c7cf79ea0b787adea
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-08-06 09:43:14 +0200

    Revert "Don't save to S3 - need manual edit"
    This reverts commit 0fd7d50e42047daf8d8bd3108627cf267aa19c61.

--- src/main/net/nosocial/germana1/ExtractPhrases.java
+++ src/main/net/nosocial/germana1/ExtractPhrases.java
@@ -2,18 +2,22 @@ package net.nosocial.germana1;
 
 import com.amazonaws.client.builder.AwsClientBuilder;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.textract.AmazonTextract;
 import com.amazonaws.services.textract.AmazonTextractClientBuilder;
 import com.amazonaws.services.textract.model.*;
 import com.amazonaws.services.textract.model.GetDocumentAnalysisResult;
 
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExtractPhrases {
     public static final String FILE_NAME = "a1-phrases.txt";
     public static final String LOCAL_PATH = "out/" + FILE_NAME;
+    public static final String S3_PATH = "goethe_de/extract/" + FILE_NAME;
 
     public static void main(String[] args) {
         System.out.println("German A1 Trainer Tool (c) 2023 by NoSocial.Net");
@@ -54,6 +58,15 @@ public class ExtractPhrases {
             e.printStackTrace();
             System.exit(1);
         }
+
+        System.out.println("Uploading phrases to S3...");
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion("eu-west-1")
+                .build();
+
+        s3Client.putObject(DownloadWordList.BUCKET_NAME, S3_PATH, new File(LOCAL_PATH));
+
         System.out.println("Done.");
     }
 
