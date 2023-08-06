f6a029ea5b53d95efd4652232256b6fcbfbf5f3c
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-08-06 11:37:10 +0200

    Translation skeleton

---
+++ src/main/net/nosocial/germana1/TranslatePhrases.java
@@ -0,0 +1,11 @@
+package net.nosocial.germana1;
+
+public class TranslatePhrases {
+
+    public static void main(String[] args) {
+        System.out.println("German A1 Trainer Tool (c) 2023 by NoSocial.Net");
+
+        System.out.println("Translating phrases with Amazon Translate...");
+
+    }
+}
