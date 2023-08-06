2fa71971dddce9d632fca2bb2950a355a9fc1650
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-08-06 10:43:17 +0200

    Getting sub speech marks

--- src/main/net/nosocial/germana1/NarratePhrases.java
+++ src/main/net/nosocial/germana1/NarratePhrases.java
@@ -12,7 +12,9 @@ import java.util.List;
 
 public class NarratePhrases {
     public static final String MP3_FILE_NAME = "a1-phrases-00.mp3";
+    public static final String JSON_FILE_NAME = "a1-phrases-00.json";
     public static final String S3_MP3_PATH = "goethe_de/narrate/" + MP3_FILE_NAME;
+    public static final String S3_JSON_PATH = "goethe_de/narrate/" + JSON_FILE_NAME;
 
     public static void main(String[] args) {
         System.out.println("German A1 Trainer Tool (c) 2023 by NoSocial.Net");
@@ -57,8 +59,17 @@ public class NarratePhrases {
         text = text.replace("'", "&apos;");
         text = text.replaceAll("\\(.+\\)", "");
         text = text.replace("\n", "\n<break time=\"5s\"/>\n");
-        text = "<speak><prosody rate=\"x-slow\">\n" + text + "</prosody></speak>";
+        text = "<speak><mark name=\"sub_start\"/><prosody rate=\"x-slow\">\n"
+                + text
+                + "</prosody><mark name=\"sub_end\"/></speak>";
 
+        synthesizeGermanAudio(polly, voice, text);
+        synthesizeGermanSpeechMarks(polly, voice, text);
+
+        System.out.println("Done.");
+    }
+
+    private static void synthesizeGermanAudio(AmazonPolly polly, Voice voice, String text) {
         SynthesizeSpeechRequest synthReq =
                 new SynthesizeSpeechRequest().withText(text).withTextType(TextType.Ssml)
                         .withVoiceId(voice.getId())
@@ -67,14 +78,29 @@ public class NarratePhrases {
         SynthesizeSpeechResult synthRes = polly.synthesizeSpeech(synthReq);
         InputStream synthStream = synthRes.getAudioStream();
 
-        // Save to S3
-        System.out.println("Saving to S3...");
+        System.out.println("Saving German audio to S3...");
 
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                 .withRegion("eu-west-1")
                 .build();
         s3Client.putObject(DownloadWordList.BUCKET_NAME, S3_MP3_PATH, synthStream, null);
+    }
 
-        System.out.println("Done.");
+    private static void synthesizeGermanSpeechMarks(AmazonPolly polly, Voice voice, String text) {
+        SynthesizeSpeechRequest synthReq =
+                new SynthesizeSpeechRequest().withText(text).withTextType(TextType.Ssml)
+                        .withSpeechMarkTypes(SpeechMarkType.Ssml)
+                        .withVoiceId(voice.getId())
+                        .withOutputFormat(OutputFormat.Json)
+                        .withEngine("neural");
+        SynthesizeSpeechResult synthRes = polly.synthesizeSpeech(synthReq);
+        InputStream synthStream = synthRes.getAudioStream();
+
+        System.out.println("Saving German speech marks to S3...");
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion("eu-west-1")
+                .build();
+        s3Client.putObject(DownloadWordList.BUCKET_NAME, S3_JSON_PATH, synthStream, null);
     }
 }
